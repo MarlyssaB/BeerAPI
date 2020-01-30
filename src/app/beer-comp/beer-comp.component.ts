fe7484cd647d839b6d8b47a55f4a23f1085fbd75
MarlyssaB <marlyssa.boucher@gmail.com>
2020-01-30 15:04:22 -0600

    initial commit

---
+++ src/app/beer-comp/beer-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beer-comp',
+  templateUrl: './beer-comp.component.html',
+  styleUrls: ['./beer-comp.component.css']
+})
+export class BeerCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
